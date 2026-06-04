517370061cfe71c06b9a857313b8aa044537bdd5
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-06-03 22:28:17 -0300

    feat: criado o formulário

---
+++ fygame/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import ViewSolicitarRecomendacao from "./views/solicitarRecomendacao";
+
+function App() {
+  return (
+    <div>
+      <ViewSolicitarRecomendacao />
+    </div>
+  );
+}
+
+export default App;
---
+++ fygame/src/views/solicitarRecomendacao.jsx
@@ -0,0 +1,15 @@
+function ViewSolicitarRecomendacao() {
+    return (
+        <div className="viewSolicitarRecomendacao">
+            <h1>Descubra sua próxima aventura</h1>
+            <form action="" method="POST">
+                <input type="text" name="jogo1"/>
+                <input type="text" name="jogo2"/>
+                <input type="text" name="jogo3"/>
+                <button type="button" onClick={}>confirmar</button>
+            </form>
+        </div>
+    )
+}
+
+export default ViewSolicitarRecomendacao;
