fcd6cd7602dc76921d151fcdeb0bd348d566be91
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-06-11 03:32:27 -0300

    backup: preparando para importar login

--- fygame/src/App.jsx
+++ fygame/src/App.jsx
@@ -10,20 +10,34 @@ function App() {
     jogo3: ''
   });
 
+  //Registra os valores do input no campo correspondente em 'jogos'
+  const salvarInput = (e) => {
+    const { name, value } = e.target; //name é o name do input && value é o value do input
+
+    setJogos((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  function GerarRecomendacao(event) {
+    //Gerar recomendação com os jogos informados
+    event.preventDefault();
+
+    console.log('Jogos informados:');
+    /*
+    console.log('Jogo 1:', jogos.jogo1);
+    console.log('Jogo 2:', jogos.jogo2);
+    console.log('Jogo 3:', jogos.jogo3);
+    */
+    console.log('Jogos: ', jogos);
+  }
+
   return (
     <div>
-      <ViewSolicitarRecomendacao enviarForm={enviarForm()} />
+      <ViewSolicitarRecomendacao GerarRecomendacao={GerarRecomendacao} salvarInput={salvarInput} />
     </div>
   );
-}
-
-function enviarForm(event) {
-  event.preventDefault();
-
-  console.log('Jogos informados:');
-  console.log('Jogo 1:', jogo1);
-  console.log('Jogo 2:', jogo2);
-  console.log('Jogo 3:', jogo3);
 }
 
 export default App;
